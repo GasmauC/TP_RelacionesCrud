9ccb85d857eb26dafb1b6163fa057df29ae1fb7b
GasmauC <gaskk21@gmail.com>
2023-10-12 01:38:59 -0300

    subiendo tp_relaciones

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,215 @@
+const path = require('path');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+const moment = require('moment')
+
+
+//Aqui tienen una forma de llamar a cada uno de los modelos
+// const {Movies,Genres,Actor} = require('../database/models');
+
+//Aquí tienen otra forma de llamar a los modelos creados
+const Movies = db.Movie;
+const Genres = db.Genre;
+const Actors = db.Actor;
+
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                return res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id,{
+            include:['genre','actors'] // tambien tre informacion del genero
+        }) 
+            .then(movie => {
+               return  res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    },
+    //Aqui dispongo las rutas para trabajar con el CRUD
+    add: function (req, res) {
+        const genres = db.Genre.findAll({
+            order: ['name']
+        })
+        const actors = db.Actor.findAll({
+          order : [
+            ['first_name'],
+            ['last_name']
+          ]
+        })
+    
+        Promise.all([genres,actors])
+        .then(([genres,actors]) => {
+           return res.render("moviesAdd", {
+            genres,
+            actors
+           }); 
+        })
+        .catch(error => console.log(error))
+        
+      },
+    create: function (req,res) {
+        const {title,rating,awards,release_date,length,genre_id} = req.body //genre_id viene de la vista, del formulario que tiene <select name="genre_id"
+        //hacemos referencia al nombre de las columnas
+         db.Movie.create({
+             title :title.trim(),
+             rating,
+             awards,
+             release_date,
+             length,
+             genre_id,
+
+         }).then((movie) => {
+            console.log(movie)
+            return res.redirect("/movies")
+         }).catch(error => console.log(error))
+    },
+    edit: function(req,res) {
+        const allGenres = db.Genre.findAll({
+            order:['name']
+        
+
+        })
+        
+        const movie = db.Movie.findByPk(req.params.id,({
+            include : ['actors']
+        }))
+        const actors = db.Actor.findAll({
+            order : [
+              ['first_name'],
+              ['last_name']
+            ]
+          })
+      
+        Promise.all([movie,allGenres,actors])
+            .then(([movie,allGenres,actors]) => {
+            
+            return res.render('moviesEdit',{
+                Movie : movie ,
+                moment,                  //la vista esperoa que se envie con mayuscula
+                allGenres,
+                actors
+            })
+
+        }).catch(error => console.log(error))
+
+    },
+    update: function (req, res) {
+      let { title, rating, awards, release_date, length, genre_id, actors } = req.body;
+      actors = typeof actors === 'string' ? [actors] : actors
+      db.Movie.update(
+        {
+          title: title.trim(),
+          rating,
+          awards,
+          release_date,
+          length,
+          genre_id
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      )
+        .then((response) => {
+          console.log(response);
+          db.Actor_Movie.destroy({
+            where: {
+              movie_id : req.params.id
+            }
+          })
+          .then(() => {
+            if (actors) {
+              const actorsDB = actors.map(actor => {
+                return {
+                  movie_id: req.params.id,
+                  actor_id: actor
+                }
+              })
+              db.Actor_Movie.bulkCreate(actorsDB, {
+                validate: true
+              })
+              .then(() => {
+                return res.redirect('/movies/detail/' + req.params.id)
+              })
+            }
+          })
+        })
+        .catch((error) => console.log(error));
+    },
+    delete: function (req, res) {
+      
+      db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+          res.render("moviesDelete.ejs", {
+            Movie: movie,
+          });
+        })
+        .catch((error) => console.log(error));
+    },
+    destroy: function (req, res) {
+      
+      // antes de eliminar la pelicula tenemos que ver si hay actores asociodos!
+      db.Actor_Movie.destroy({
+        where: {
+          movie_id: req.params.id,
+        },
+      })
+        .then((response) => {
+          console.log("response Actor_Movie => ", response);
+          db.Actor.update(
+            {
+              favorite_movie_id: null,
+            },
+            {
+              where: {
+                favorite_movie_id: req.params.id,
+              },
+            }
+          ).then((response) => {
+            console.log("response Actor => ", response)
+            db.Movie.destroy({
+              where: {
+                id: req.params.id,
+              },
+            }).then((response) => {
+              console.log("response Movie => ", response)
+              return res.redirect("/movies")
+            })
+          })
+        })
+        .catch((error) => console.log(error))
+    },
+}
+
+module.exports = moviesController
+
+//.finally() siempre se ejecuta
---
+++ src/database/models/Actor_Movie.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Actor_Movie';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull:false,
+            autoIncrement: true
+        },
+        actor_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull:false,
+
+        },
+        movie_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull:false,
+        }
+    };
+    let config = {
+        tableName: 'actor_movie',
+        timestamps: true,
+        createdAt:'created_at',
+        updatedAt:'updated_at',
+        deletedAt:false,
+        tableName: 'actor_movie'
+    };
+    const Actor_Movie = sequelize.define(alias, cols, config)
+
+    return Actor_Movie
+}
